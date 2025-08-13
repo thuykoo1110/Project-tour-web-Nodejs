ffff8b216fa91fe2b3f1e223c2e1943ee79edc67
Thuy <caiphuongthuy1110@gmail.com>
2025-08-13 21:00:57 +0700

    Tích hợp PUG

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express') //nhúng thư viện/hàm vào file này (như import)
 const app = express()
+const path = require('path')
 const port = 3000
 
+// Thiết lập thư mục chứa code giao diện (view)
+// app.set('views', './views') //./views: views dỉrectory, local ổn nhưng đẩy lên onl thì ko
+app.set('views', path.join(__dirname, "views"));
+//Thiết lập template engine (vd Pug)
+app.set('view engine', 'pug');
+
+//Thiết lập thư mục chứa file tĩnh (static file)
+app.use(express.static(path.join(__dirname, "public"))); //vì chạy trên onl ko hỉu nên mới có path join
 app.get('/', (req, res) => {
-    res.send('Trang chủ')
+    res.render('client/pages/home', {
+        pageTitle: 'Trang chủ'
+    }) //convert pug file to html
 }) //'/'-trang chủ
 
 app.get('/tour', (req, res) => {
-    res.send('Danh sách tour')
+    res.render('client/pages/tour-list', {
+        pageTitle: 'Danh sách tour'
+    })
 })
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
