71b66e9cdb7306aa6fc113aca958eeeedb8e74f3
Thuy <caiphuongthuy1110@gmail.com>
2025-08-15 20:35:05 +0700

    controller-mvc model

---
+++ controllers/client/tour.controller.js
@@ -0,0 +1,9 @@
+const Tour = require("../../models/tour.model");
+module.exports.list = async (req, res) => {
+    const tourList = await Tour.find({}); //lấy document trong collections tours
+    // console.log(tourList);
+    res.render('client/pages/tour-list', {
+        pageTitle: 'Danh sách tour',
+        tourList: tourList
+    })
+}
--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const express = require('express') //nhúng thư viện/hàm vào file này (như import)
 const mongoose = require('mongoose');
-require('dotenv').config()
+require('dotenv').config();
 const app = express()
 const path = require('path')
 const port = 3000
-
+const homeController = require('./controllers/client/home.controller');
+const tourController = require('./controllers/client/tour.controller')
 //kết nối database
 mongoose.connect(process.env.DATABASE);
 
-const Tour = mongoose.model('Tour',
-    {
-        name: String,
-        vehicle: String
-    },
-    "tours"
-);
 
 // Thiết lập thư mục chứa code giao diện (view)
 // app.set('views', './views') //./views: views dỉrectory, local ổn nhưng đẩy lên onl thì ko
@@ -24,20 +18,10 @@ app.set('view engine', 'pug');
 
 //Thiết lập thư mục chứa file tĩnh (static file)
 app.use(express.static(path.join(__dirname, "public"))); //vì deploy onl lỗi nên mới có path join
-app.get('/', (req, res) => {
-    res.render('client/pages/home', {
-        pageTitle: 'Trang chủ'
-    }) //convert pug file to html
-}) //'/'-trang chủ
+app.get('/', homeController.home) //'/'-trang chủ
 
-app.get('/tour', async (req, res) => {
-    const tourList = await Tour.find({});
-    // console.log(tourList);
-    res.render('client/pages/tour-list', {
-        pageTitle: 'Danh sách tour',
-        tourList: tourList
-    })
-}) //app.get(path, callback)
+app.get('/tour', tourController.list)
+//app.get(path, callback)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
