95b0d6a173f7ca8a9976c65095d12234d58082c1
Thuy <caiphuongthuy1110@gmail.com>
2025-08-15 20:17:24 +0700

    create database

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express') //nhúng thư viện/hàm vào file này (như import)
+const mongoose = require('mongoose');
+require('dotenv').config()
 const app = express()
 const path = require('path')
 const port = 3000
+
+//kết nối database
+mongoose.connect(process.env.DATABASE);
+
+const Tour = mongoose.model('Tour',
+    {
+        name: String,
+        vehicle: String
+    },
+    "tours"
+);
 
 // Thiết lập thư mục chứa code giao diện (view)
 // app.set('views', './views') //./views: views dỉrectory, local ổn nhưng đẩy lên onl thì ko
@@ -17,12 +30,17 @@ app.get('/', (req, res) => {
     }) //convert pug file to html
 }) //'/'-trang chủ
 
-app.get('/tour', (req, res) => {
+app.get('/tour', async (req, res) => {
+    const tourList = await Tour.find({});
+    // console.log(tourList);
     res.render('client/pages/tour-list', {
-        pageTitle: 'Danh sách tour'
+        pageTitle: 'Danh sách tour',
+        tourList: tourList
     })
 }) //app.get(path, callback)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+// caiphuongthuy1110 zYaFDpZqNNCGbXK5: mongodb database user
