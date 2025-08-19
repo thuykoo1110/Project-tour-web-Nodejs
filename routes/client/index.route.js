7f5f2089ab4b8359cd31b3114a1113f58fde4bd4
Thuy <caiphuongthuy1110@gmail.com>
2025-08-19 21:14:39 +0700

    Cart integration

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const homeRoutes = require("./home.route")
 const tourRoutes = require("./tour.route")
+const cartRoutes=require('./cart.route')
 
 router.use('/', homeRoutes)
 router.use('/tour', tourRoutes)
-
+router.use('/cart',cartRoutes)
 module.exports = router
