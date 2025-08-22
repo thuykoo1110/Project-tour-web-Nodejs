88840215d42e3cd4aedf0373689d422d1aee9b24
Thuy <caiphuongthuy1110@gmail.com>
2025-08-22 20:41:31 +0700

    validation backend

--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -1,10 +1,11 @@
 const router=require('express').Router()
 const accountController=require('../../controllers/admin/account.controller')
+const accountValidate=require('../../validate/admin/account.validate')
 
 router.get('/login',accountController.login)
 
 router.get('/register',accountController.register)
-router.post('/register',accountController.registerPost)
+router.post('/register',accountValidate.registerPost ,accountController.registerPost) //accountvalidate: hàm trung gian
 
 router.get('/forgot-password',accountController.forgotPassword)
 router.get('/otp-password',accountController.otpPassword)
