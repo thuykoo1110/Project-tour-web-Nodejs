be109646ec6eb109830c4f31d4360c06cd1d20a0
Thuy <caiphuongthuy1110@gmail.com>
2025-09-29 13:22:57 +0700

    làm khối breadcrumb ở tour detail

--- controllers/client/tour.controller.js
+++ controllers/client/tour.controller.js
@@ -1,7 +1,49 @@
 const Tour = require("../../models/tour.model");
+const Category = require('../../models/catagory.model')
 
 module.exports.detail = async (req, res) => {
+  const slug = req.params.slug;
+  const tourDetail = await Tour
+    .findOne({
+      slug: slug,
+      deleted: false,
+      status: "active"
+    })
+
+  if(!tourDetail){
+    res.redirect('/');
+    return;
+  }
+
+   // Breadcrumb
+  const breadcrumb = [];
+
+  if(tourDetail.category) {
+    const category = await Category
+      .findOne({
+        _id: tourDetail.category,
+        deleted: false,
+        status: "active"
+      })
+
+    if(category) {
+      breadcrumb.push({
+        name: category.name,
+        slug: category.slug,
+        avatar: category.avatar,
+      });
+    }
+  }
+
+  breadcrumb.push({
+    name: tourDetail.name,
+    slug: tourDetail.slug,
+    avatar: tourDetail.avatar,
+  });
+  // End Breadcrumb
+
   res.render('client/pages/tour-detail', {
-    pageTitle: "Chi tiết tour"
+    pageTitle: tourDetail.name,
+    breadCrumb: breadcrumb
   })
 }
