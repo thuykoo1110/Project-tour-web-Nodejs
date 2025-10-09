bcb1d6eb115909345fe2671837cbdeb7461818f5
Thuy <caiphuongthuy1110@gmail.com>
2025-10-09 21:45:39 +0700

    làm khối thogn số tổng quan

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,27 @@
+const AccountAdmin = require('../../models/account-admin.model')
+const Order = require('../../models/order.model')
+
 module.exports.dashboard=async (req,res)=>{
+  // thông số tổng quan
+  const overview = {
+    totalAdmin: 0,
+    totalOrder: 0,
+    totalRevenue: 0
+  }
+
+  overview.totalAdmin = await AccountAdmin.countDocuments({
+    deleted: false
+  })
+
+  const orderList = await Order.find({
+    deleted: false
+  })
+  overview.totalOrder = orderList.length;
+  overview.totalRevenue = orderList.reduce((total,item) => total + item.total,0);
+  
+  // End thông số tổng quan
   res.render('admin/pages/dashboard',{
-    pageTitle: "Dashboard"
+    pageTitle: "Dashboard",
+    overview: overview
   })
 }
